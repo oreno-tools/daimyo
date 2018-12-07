9d587d33ac65b9c7897015f76eb5b71732ecab63
inokappa <inokara@gmail.com>
2018-12-07 12:02:53 +0900

    project id を指定していない場合に, とりあえず raise するようにした.

--- lib/daimyo/cli.rb
+++ lib/daimyo/cli.rb
@@ -10,6 +10,7 @@ module Daimyo
     desc 'list', 'Listing wiki page in project.'
     option :project_id, type: :string, aliases: '-p', desc: 'Specify the project id or project key'
     def list
+      raise 'Backlog プロジェクトキーを指定してください.' if options[:project_id].nil?
       d = Daimyo::List.new
       d.run(options[:project_id])
     end
@@ -18,6 +19,7 @@ module Daimyo
     option :project_id, type: :string, aliases: '-p', desc: 'Specify the project id or project key'
     option :wiki_id, type: :string, aliases: '-i', desc: 'Specify the wiki id'
     def export
+      raise 'Backlog プロジェクトキーを指定してください.' if options[:project_id].nil?
       d = Daimyo::Export.new
       d.run(options[:project_id], options[:wiki_id])
     end
@@ -26,6 +28,7 @@ module Daimyo
     option :project_id, type: :string, aliases: '-p', desc: 'Specify the project id or project key'
     option :dry_run, type: :boolean, aliases: '-d', desc: 'Dryrun.'
     def publish
+      raise 'Backlog プロジェクトキーを指定してください.' if options[:project_id].nil?
       d = Daimyo::Publish.new(options)
       d.run(options[:project_id], options[:dry_run])
     end
